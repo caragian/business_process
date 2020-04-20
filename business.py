6d1c1117d265c968f11ccbc446538f936f6ac4e0
root <root@master.bz.lan>
2020-04-20 15:28:09 +0200

    Comment File

--- business.py
+++ business.py
@@ -1,4 +1,4 @@
-# Generation new Business Process
+# Generation / Update Business Process
 #
 # /neteye/shared/icingaweb2/conf/modules/businessprocess/processes
 #
@@ -9,20 +9,21 @@ import os
 import argparse
 import sys
 import logging
-import linecache
 import stat
 import re
 import in_place
 
 logging.basicConfig(level=logging.DEBUG)
 
-parser = argparse.ArgumentParser(description='Bussiness Process Tutorial || For more info and examples type "python3 business.py"')
-parser.add_argument('-b', '--business', dest='process', required=True, type=str, help='Choose Your Config Business Process Name')
-parser.add_argument('-p', '--parent',  dest='parent', required=True, type=str, help='Choose your Node Master')
-parser.add_argument('-a', '--aggregator' , dest='aggregator', required=True, type=str, help='Choose your aggregatorimal Activity Service')
+# Declare Arguments
+parser = argparse.ArgumentParser(description='Bussiness Process Tutorial')
+parser = argparse.ArgumentParser(description='Example: python3 business.py -b Windows -p Windows_ESX -a AND -t template_esx.txt -f hosts_esx.txt')
+parser.add_argument('-b', '--business', dest='process', required=True, type=str, help='Choose Your Config Business Process File (Without *.conf)')
+parser.add_argument('-p', '--parent',  dest='parent', required=True, type=str, help='Choose your Node Parent')
+parser.add_argument('-a', '--aggregator' , dest='aggregator', required=True, type=str, help='Choose your aggregator Activity Service (AND, OR, MIN[1-9])')
 parser.add_argument('-t', '--template', dest='template', required=True, type=str, help='Choose your Host Template')
 parser.add_argument('-f', '--host' , dest='host', required=True, type=str, help='Choose your Hosts File')
-# parser.add_argument('--tutorial' , dest='tutorial', required=False, help='For more info and examples type "python3 business.py"', action='store_true')
+
 
 args = parser.parse_args()
 
@@ -59,23 +60,21 @@ header = '### Business Process Config File ###\n#\n# Title           : '+process
 #     print("-t | --template     Choose your Host Template")
 #     print("-f | --host         Choose your Hosts File\n")
 
+
+# Change Permission to Conf File
 def permission(process):
-    #change owner
+    # change owner
     filename = path_process
     st = os.stat(filename)
     usr_own = st.st_uid
     grp_own = st.st_gid
-
-    #usr_own = 48    #apache
-    #grp_own = 999   #icingaweb2
-
-    #change mode
-    f_mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP      #file mode
+    # file mode
+    f_mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP      
 
     os.chown(process, usr_own, grp_own)
     os.chmod(process, f_mode)
 
-
+# Generate template for Hosts List
 def generate_temp(hostname,template):
     host_t1 = linecache.getline(template,1)
     host_t2 = linecache.getline(template,2)
@@ -85,17 +84,17 @@ def generate_temp(hostname,template):
 
     return(host_1, host_2)
 
+# Parse Hosts in TXT File to List (host_list)
 def parse_host(hosts_file, host_list):
     with open (hosts_file, 'r') as myfile:
         for line in myfile:
             if line != '\n':
                 host = line.strip('\n')
                 host_list.append(host)
-
-
     myfile.close
     return(host_list)
 
+# Generate Parent with Host List and choose Aggregator
 def create_parent(parent, aggregator, host_list):
     if aggregator == 'and':
         parent_1 = parent + ' = ' + ' & '.join(host_list)
@@ -111,7 +110,9 @@ def create_parent(parent, aggregator, host_list):
         parent_1 = parent + ' = ' + match.group(1) + ' of: ' + ' + '.join(host_list)
     return(parent_1, parent_2)
 
+# Check if File Conf exists
 def check_exists(file):
+    # Creation File and Write Header
     if not os.path.exists(file):
         with open(file, 'w') as title:
             os.utime(file, None)
@@ -120,15 +121,14 @@ def check_exists(file):
             title.close
             status = 'write'
             permission(process)
+    #Open File in Append Mode
     elif os.path.exists(file):
             status = 'append'
     return(status)
 
-
+# Write Parent and Host List if not exists
 def write_file(process,host_list, parent_1, parent_2):
-
     process = open(process, 'a')
-
     process.write(parent_1)
     process.write('\n')
     process.write(parent_2)
@@ -143,9 +143,8 @@ def write_file(process,host_list, parent_1, parent_2):
 
     process.close()
 
-
+# Adding new Hosts in Append Mode
 def write_host(process,host_1, host_2):
-        print('Im am in Write_Host')
         print("Host1 is {n} || Host2 is {s}".format(n=host_1, s=host_2))
         process = open(process, 'a')
 
@@ -155,20 +154,7 @@ def write_host(process,host_1, host_2):
 
         process.close()
 
-
-
-# def update_parent(process, parent, parent_1):
-#     with open(process, 'r') as f:
-#         data = f.readlines()
-#         for line in data:
-#             parent = parent + ' ='
-#             if parent in line:
-#                 line = 'ciao'
-#                 with open(process, 'a') as a:
-#                     a.write(line.replace(line, parent_1))
-#                     break
-
-
+# Update Parent Line or Write New Parent with Hosts
 def add_parent(process, parent, host_list, parent_1, parent_2):
     with open(process) as f:
             file_read = f.read()
@@ -179,33 +165,40 @@ def add_parent(process, parent, host_list, parent_1, parent_2):
                 # update_parent(process, parent, parent_1)
                 with in_place.InPlace(process) as fp:
                     for line in fp:
+                        # If parent it's in this Line: Replace Line with New Line (containt also new Hosts)
                         if parent in line:
                             line = line.replace(line, parent_1)
                             fp.write(line)
                             fp.write('\n')
+                        # If parent it's now in this Line: copy Line to New File
                         elif parent not in line:
                             fp.write(line)
+            # If parent not exists in File: Create New Parent with his Hosts
             elif parent is not file_read:
                 write_file(process,host_list,parent_1,parent_2)
 
-
+# Update Hosts with new Template or Write New Hosts
 def add_host(process, host_list, template_file):      
     print(host_list)
     with open(process) as f:
         file_read = f.read()
         for host in host_list:
             host_1, host_2 = generate_temp(host,template_file)
+            # Check if Host is in the File
             if host in file_read:     
                 print(host + ' is in File')
+                # Replace Line with New Template
                 with in_place.InPlace(process) as fp:
                     for line in fp:
                         match = re.match(r"{}\s+\=".format(host), line)
-                        # print(bool(match))
+                        # If it' s in this Line : Replace with new Line
                         if bool(match) is True:
                             line = line.replace(line, host_1)
                             fp.write(line)
+                        # It it's not in this Line : copy Line to new File
                         elif bool(match) is False:
                             fp.write(line)
+            # Add New Host
             elif host is not file_read:
                 write_host(process,host_1, host_2)
           
@@ -223,20 +216,14 @@ def add_host(process, host_list, template_file):
         
 #MAIN
 
-# The list of command line arguments passed to a Python script. argv[0] is the script name. So:
-# if tutorial is not None:
-#     helpOption()
-#     sys.exit(1)
-
+# Reading Input
 host_list = parse_host(hosts_file, host_list)
 parent_1, parent_2 = create_parent(parent, aggregator, host_list)
+
 os.chdir(path_process)
 
- 
+# Host List
 print('Hosts = ' + str(host_list)[1:-1])
-
-
-
 
 status = check_exists(process)
 if status == 'write':
